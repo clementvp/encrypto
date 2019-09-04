a3f09ab9981b070454b725be1c3da65653678911
Van Peuter Clément <clement.vanpeuter@gmail.com>
2019-09-04 21:45:38 +0200

    Begin store implementation in front (#6)
    * :sparkles: add the "old friend" unseal vault methods
    
    * :sparkles: write empty methods for the store

--- src/renderer/store.js
+++ src/renderer/store.js
@@ -4,7 +4,28 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {}
+  state: {
+    hash: "",
+    iv: "",
+    salt: "",
+    data: ""
+  },
+  mutations: {
+    setHash(state, hash) {
+      state.hash = hash;
+    },
+    setData(state, data) {
+      state.data = data;
+    },
+    setIv(state, iv) {
+      state.iv = iv;
+    },
+    setSalt(state, salt) {
+      state.salt = salt;
+    }
+  },
+  actions: {
+    encryptAndStoreDb() {},
+    decryptAnStoreState() {}
+  }
 });
